6003150d3c0e026c299694ac241c16015625fb88
Dewey Hong <doohee323@gmail.com>
2013-10-20 23:41:06 +0900

    port changed

--- app.js
+++ app.js
@@ -20,25 +20,20 @@ var Room = io
     routes = data;
     
 //    socket.emit('centers', "you've joined " + data);
-    socket.broadcast.emit('centers', 'someone joined room');
+    socket.broadcast.emit('centers', 'someone joined');
   }); 
   socket.on('insert', function(data) {
-    if (routes) {
-    	console.log('0000000000001');
+	console.log('0000000000001');
     	// 그냥 socket.io 만 쓸 때
-    	socket.broadcast.emit('inserted', data);
-    } else {
-    	console.log('0000000000002' + data);
-    	socket.send('login first');
-    }
+	socket.broadcast.emit('inserted', data);
   });
   socket.on('update', function(data) {
-	console.log('0000000000003');
+	console.log('0000000000002');
 	// 그냥 socket.io 만 쓸 때
 	socket.broadcast.emit('updated', data);
   });
   socket.on('delete', function(data) {
-	console.log('0000000000005');
+	console.log('0000000000003');
 	socket.emit('deleted', data);
 	socket.broadcast.emit('deleted', data);
   });
@@ -47,7 +42,7 @@ var Room = io
 console.log('__dirname:' + __dirname);
 app.use(express.static(__dirname + ''));
 
-server.listen(8080);
+server.listen(7002);
 
 
 // redis를 쓸 
